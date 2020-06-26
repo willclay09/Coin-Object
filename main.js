5a43e391ede66dbfeeb2b53495b1075d76072eff
William <william_6426@hotmail.com>
2020-06-26 15:10:16 -0500

    finished project

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
 let coin = {
     state: 0,
     flip: function() {
-        /* 1. Randomly set your coin object's "state" property to be either 
-           0 or 1: use "this.state" to access the "state" property on this object.*/
+        this.state = Math.round(Math.random(0,1))
+    
     },
     toString: function() {
-        /* 2. Return the string "Heads" or "Tails", depending on whether
-           "this.state" is 0 or 1. */
+       if(this.state === 0) {
+           document.body.append('heads ')
+       } else {
+        document.body.append('tails ')
+       }
     },
     toHTML: function() {
+        parentElement = document.querySelector('body')
         let image = document.createElement('img');
-        /* 3. Set the properties of this image element to show either face-up
-           or face-down, depending on whether this.state is 0 or 1.*/
+        
+        if(this.state === 0) {
+            image.src = './img/heads.jpg'
+        } else {
+            image.src = './img/tails.jpg'
+        }
+        parentElement.append(image)
         return image;
     }
 };
+function display20Flips(){
+    for(let indexFlips = 0; indexFlips < 20; indexFlips += 1){
+    coin.flip()
+    coin.toString()
+    }
+}
+function display20Images(){
+    for(let indexImages = 0; indexImages < 20; indexImages += 1){
+    coin.flip()
+    coin.toHTML()
+    }
+}
+display20Flips()
+display20Images()
+
+
+// fix flip function
+// run display20Flips 20 times
+// run display20Images 20 times
